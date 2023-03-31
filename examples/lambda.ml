111397d53aa38a5e31b13390e75661cb180d8355
Christopher Armstrong <chris-armstrong@users.noreply.github.com>
2023-03-31 22:15:05 +1100

    fix example

--- examples/lambda.ml
+++ examples/lambda.ml
@@ -1,8 +1,14 @@
 open Cf_aws.Lambda
 
-let f =
-  Function.make
-    ~code:(Function.make_code ~s3_bucket:"bucket" ~s3_key:"my_key/key" ())
-    ~role:"role" ()
+let main () =
+  let f =
+    Function.make
+      ~runtime:"nodejs12.x"
+      ~code:(Function.make_code ~s3_bucket:"bucket" ~s3_key:"my_key/key" ())
+      ~role:"role" () in
 
-let x = Function.yojson_of_t f
+  let x = Function.yojson_of_t f in
+
+  Format.printf "%s\n" (Yojson.Safe.to_string x)
+
+let () = main ()
