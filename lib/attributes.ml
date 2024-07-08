d4c3c1340a98d9a4b518a87e323ddc088bb22b05
Chris Armstrong <chris-armstrong@users.noreply.github.com>
2024-07-08 20:55:57 +1000

    chore: format

--- lib/attributes.ml
+++ lib/attributes.ml
@@ -3,13 +3,20 @@ open Token_map
 (** Create a [Ref] resolver token string for the specified [logical_id] *)
 let ref_resolver logical_id =
   let module RefResolvable = struct
-    let resolve () = `Assoc [("Ref", `String logical_id)]
+    let resolve () = `Assoc [ ("Ref", `String logical_id) ]
   end in
-  (module RefResolvable: Resolvable)
+  (module RefResolvable : Resolvable)
 
-(** Create a [Fn::GetAtt] resolver token string for the specified [logical_id] and [attribute_name] *)
+(** 
+    @param logical_id
+    @param attribute_name
+
+    Create a [Fn::GetAtt] resolver token string for the specified [logical_id] and [attribute_name] 
+*)
+
 let attr_resolver logical_id attribute_name =
   let module AttrResolvable = struct
-    let resolve () = `Assoc [("Fn::GetAtt", `String (logical_id ^ "." ^ attribute_name))]
+    let resolve () =
+      `Assoc [ ("Fn::GetAtt", `String (logical_id ^ "." ^ attribute_name)) ]
   end in
-  (module AttrResolvable: Resolvable)
+  (module AttrResolvable : Resolvable)
